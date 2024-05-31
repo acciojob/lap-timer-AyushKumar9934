05f1380970927abedf6d1d6960f03e166f6abb4c
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-31 17:57:34 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,57 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState,useRef } from 'react'
 
 const App = () => {
+   const [trackingTime,setTrackingTime]=useState(0);
+   const [lapItem,SetLapItem]=useState([]);
+   let intervalTime=useRef();
+   
+   useEffect(()=>{
+    return ()=> clearInterval(intervalTime.current);
+   },[])
+  function handleStart(){
+    clearInterval(intervalTime.current);
+    if(!intervalTime.current){
+    intervalTime.current=setInterval(()=>{
+      setTrackingTime((prevTime)=>prevTime+1);
+    },1);}
+    
+
+  }
+  function handleStop(){
+    if(intervalTime.current){
+    clearInterval(intervalTime.current);
+    intervalTime.current=null;}
+  }
+  function handleLap() {
+    SetLapItem((prevItem) => [...prevItem, trackingTime])
+  }
+  function handleReset(){
+    clearInterval(intervalTime.current);
+    intervalTime.current=null
+    setTrackingTime(0);
+    SetLapItem([]);
+
+
+  }
+  function pad(number) {
+    // add a leading zero if the number is less than 10
+    return (number < 10 ? "0" : "") + number;
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+         {`${pad(Math.floor(trackingTime / 3600))}:${pad(Math.floor((trackingTime / 60) % 60))}:${pad(trackingTime % 60)}`}
+         <div>
+         The lap items are :<ul>{lapItem.map((item,index)=>{
+        return <li> <p key={index}> {`${pad(Math.floor(item / 3600))}:${pad(Math.floor((item / 60) % 60))}:${pad(item % 60)}`}</p></li>
+         })}    </ul> </div>
+        
+         
+        <button onClick={handleStart} >Start</button>
+        <button onClick={handleStop} >Stop</button>
+        <button onClick={handleLap} >Lap</button>
+        <button onClick={handleReset} >Reset</button>
+
     </div>
   )
 }
